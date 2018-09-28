8763ad93f6b526d299c9ab7fa9d554a462f10399
Dimitri King <Dimitri.king.2022@student.lhschools.org>
2018-09-28 07:33:49 -0400

    Updated version of number guessing game.

--- src/NumberGuessingGame.java
+++ src/NumberGuessingGame.java
@@ -4,50 +4,76 @@ public class NumberGuessingGame {
     public static void main(String[] args) {
 
         Scanner keyboard;
-        int pickedNumber;
+        int pickedNumber = 0;
         int guessedNumber;
         int tries;
         int games;
         String answer;
+        String type;
+        int maxTries = 0;
 
 
 
 
         keyboard = new Scanner(System.in);
-        System.out.println("Would you like to play the number guessing game?");
-        answer =  keyboard.nextLine().toLowerCase();
         games = 0;
 
 
-         while (answer.equals("yes")) {
-            pickedNumber = (int) (Math.random() * 100);
-            System.out.println("Guess the number. It is between 1 and 100.");
+         do {
+            System.out.println("This is the Number Guessing game! Would you like to play to" +
+                    " 100, 1000, or the hardest... 1000000? Key: Type 1 for 100" +
+                    ", 2 for 1000, or 3 for 1000000.");
+            type = keyboard.next();
+            while(!type.equals("1")&&!type.equals("2")&&!type.equals("3")) {
+                 System.out.println("Please type 1, 2, or 3 only.");
+                 type = keyboard.next();
+             }
+            if (type.equals("1")) {
+                pickedNumber = (int) (Math.random() * 100);
+                System.out.println("Guess the number. It is between 1 and 100.");
+                maxTries = 5;
+            }
+            if (type.equals("2")) {
+                pickedNumber = (int) (Math.random() * 1000);
+                System.out.println("Guess the number. It is between 1 and 1000.");
+                maxTries = 11;
+            }
+            if (type.equals("3")) {
+                pickedNumber = (int) (Math.random() * 1000000);
+                System.out.println("Guess the number. It is between 1 and 1000000.");
+                maxTries = 100;
+            }
+
             guessedNumber = keyboard.nextInt();
             tries = 1;
-
             while (pickedNumber != guessedNumber) {
+                if (maxTries == tries) {
+                    System.out.println("You did not guess the number.");
+                    break;
+                }
                 if (guessedNumber > pickedNumber) {
                     System.out.println("Too high");
                 }
-                else if (guessedNumber < pickedNumber) {
+                else  {
                     System.out.println("Too low");
                 }
                 System.out.println("Try again.");
                 guessedNumber = keyboard.nextInt();
                 tries = tries + 1;
+
             }
-            if (pickedNumber == guessedNumber) {
+            if (pickedNumber == guessedNumber){
                 System.out.println("You are correct.");
+                System.out.println("It took you " + tries + " tries to guess the number.");
             }
 
-            System.out.println("It took you " + tries + " tries to guess the number.");
-            System.out.println("Would you like to play again? (only type yes or no)");
+            System.out.println("Would you like to play again? (only type yes or no) ");
             answer = keyboard.next().toLowerCase();
             games += 1;
 
 
 
-        }
+        }while(answer.equals("yes"));
         if (!answer.equals("yes")) {
             System.out.println("You played a total of " + games + " time(s).");
             keyboard.close();
